7f34548d1f0fab194613290a0f9f478256dc678a
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-12-29 17:38:52 +0100

    update version number

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
   expo: {
     name: "ChaldeanNumbers",
     slug: "ChaldeanNumbers",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
